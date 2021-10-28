0e199e5ad042128b177a57f6e59ba9b238a9817b
Sergio Pallet <sergiopallet@gmail.com>
2021-10-28 16:46:32 -0300

    Changing list for table in pokemonList

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,14 +1,49 @@
-export const PokemonList = ({ pokemons }) => {
+import "./styles.css";
+
+const columns = [
+  { accessor: "name", label: "Nome" },
+  { accessor: "url", label: "url" },
+];
+
+const Table = ({ columns, rows }) => {
   return (
-    <ul className="pokemon-list">
-      {pokemons.map((pokemon) => (
-        <li key={pokemon.name}>
-          <p>{pokemon.name}</p>
-          <a href={pokemon.url} target="_blank">
-            {pokemon.url}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => {
+            return <th key={column.accessor}>{column.label}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row) => {
+          return (
+            <tr key={row.id}>
+              {columns.map((column) => {
+                return (
+                  <td key={column.accessor}>
+                    {column.accessor === "url" ? (
+                      <a
+                        href={row[column.accessor]}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {row[column.accessor]}
+                      </a>
+                    ) : (
+                      row[column.accessor]
+                    )}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
+};
+
+export const PokemonList = ({ pokemons }) => {
+  return <Table rows={pokemons} columns={columns} />;
 };
